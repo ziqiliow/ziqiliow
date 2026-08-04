0654355d6321182565b8266096a8c49a492985fa
Zi Qi Liow <ziqiliow@Zi-Qis-Air.lan>
2026-08-04 16:16:45 -0400

    Add Framer rewrite rule

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://ziqiliow.framer.website/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
